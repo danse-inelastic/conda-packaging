08ef6ce70634d09141b0e25399e422601aa0c940
Jiao Lin <jiao.lin@gmail.com>
2016-05-08 09:14:51 -0400

    added first impl. not tested yet

---
+++ make_meta.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+name = "${PKG_NAME}"
+version = "${VERSION}"
+git_clone_url = "${GITHUB_URL}.git"
+
+build_deps = "${CONDA_PKGING_BUILD_DEPS}".split()
+build_deps.append('cmake')
+run_deps = "${CONDA_PKGING_RUN_DEPS}"
+
+build_requirements = '\n'.join("    - %s" % d for d in build_deps)
+run_requirements = '\n'.join("    - %s" % d for d in run_deps)
+
+import_test = "${PKG_NAME}"
+home_url = "${HOME_URL}"
+
+template = open("meta.yaml.template").read()
+content = template % locals()
+open("meta.yaml", 'wt').write(content)
